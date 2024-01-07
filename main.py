8c8c20699e29de0dea1e27bfd0e1d0f587b9e97b
mtsspk <138814069+mtsspk@users.noreply.github.com>
2024-01-06 21:19:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -166,6 +166,8 @@ def sentiment_analysis(developer: str):
 
 @app.get("/get_recommendations/{game_id}")
 def get_recommendations(game_id: str, num_recommendations: int = 5):
+    game_id = str(game_id)
+    
     game_row = df_games_similarity.loc[game_id]
     
     similar_games = game_row.sort_values(ascending=False).index.tolist()
